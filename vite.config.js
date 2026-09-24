64766fc68b997b2813eab24c0c0a016726357a19
25104039-hub <25104039@nec.edu.in>
2026-09-24 11:56:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/Fortvexa6578/',
   plugins: [react()],
   server: {
     port: 5173,
